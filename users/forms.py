2071291403342c9ac12ac04a874bfe25503f63b7
Mridul11 <mridulmishra2991@gmail.com>
2020-04-29 17:09:13 +0530

    modified default profile pic and overriden save method in models

--- users/forms.py
+++ users/forms.py
@@ -6,15 +6,12 @@ from .models import Profile
 # for using django inbuilt UserCreationForm
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField()
-
     class Meta:
         model = User 
         fields = ['username', 'email', 'password1', 'password2']
 
-
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
-
     class Meta:
         model = User 
         fields = ['username', 'email']
